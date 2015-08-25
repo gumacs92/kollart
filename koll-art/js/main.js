cb59fd283562f1e131b72ebbb1ce54f189e2a420
gumacs92 <gguzmics01@gmail.com>
2015-08-25 08:44:52 +0200

    contact fix

--- koll-art/js/main.js
+++ koll-art/js/main.js
@@ -2,53 +2,6 @@
  * Created by gguzmics01 on 2015.07.07..
  */
 
-//----------------------------------Segédfüggvények amiket az alsó függvények hívnak meg--------------------------------
-
-/*
- function validateEmail(email) {
- var regex = "/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i";
- return re.test(email);
- }*/
-
-var success = true;
-
-function onbtnclick() {
-    //alert($("#message").val());
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var text = $("#message").val();
-
-
-    if (name == "") {
-        $("#namebox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
-        $("#namediv").attr("class", "form-group has-error has-feedback");
-        success = false;
-    } else {
-        $("#namebox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
-        $("#namediv").attr("class", "form-group has-success has-feedback");
-    }
-    if (email == "") {
-        $("#emailbox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
-        $("#emaildiv").attr("class", "form-group has-error has-feedback");
-        success = false
-    } else {
-        $("#emailbox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
-        $("#emaildiv").attr("class", "form-group has-success has-feedback");
-    }
-    if (text == "" || text == "Ide írd az üzeneted...") {
-        $("#textbox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
-        $("#textdiv").attr("class", "form-group has-error has-feedback");
-        success = false;
-    } else {
-        $("#textbox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
-        $("#textdiv").attr("class", "form-group has-success has-feedback");
-    }
-
-    $('form').on('submit', function () {
-        return success;
-    });
-}
-
 function adjust_textarea(h) {
     h.style.height = "20px";
     h.style.height = (h.scrollHeight) + "px";
@@ -104,35 +57,12 @@ function addZoomIn(reftomb) {
  }
  }*/
 
-//-----------------------------------------HTML oldalakhoz tartozó függvények-------------------------------------------
-
-//esetleg külön js fileokba átrakni a külön oldalakhoz tartozó js kódokat... <<<< DONE
-//Tipp: összefogni pagenként egy függvénybe a pagehez tartozó függvényeket és meghívni az adott oldalon egy-egy
-//script tag között így: $(document).ready(function(){ aktualisHTMLfgv(); })
-
-//külön js filokba szednia különböző htmlekhez tartozó js kódokat, mert így hibát dob....
-//^^^^^^ megcsinálatam, de ehhez be kellett rakni konkrét script részeket a htmlbe^^^^^^
-//Megjegyzés: szerintem így viszont sokkal átláthatóbb lett az egész
-/*
- $(document).ready(function () {});
- $(window).scroll(function () {});
- $(window).bind("onresize", function(){});
- */
-
 //betöltő függvény minden page-re
 $(window).load(function () {
     //beincludeolas
     $("#header").load("header.html");
     $("#footer").load("footer.html");
 });
-
-//function showIndex(){
-//    $('.content').css({display: "auto"});
-//}
-
-function index() {
-    //setTimeout("showIndex()", 4000);
-}
 
 var reftomb;
 var loaded = false;
@@ -146,75 +76,83 @@ function reference() {
 }
 
 //TODO átírni ezt az egész szennyett itt lentebb, mert ez használhatatlan
+//csak akkor kerdezz errol a formedvenyrol, ha eleg reszeg vagy
 function contactajaxsuccess(data) {
     if (data.errors.name) {
-        if($("#namebox").hasClass("glyphicon-ok")){
+        if ($("#namebox").hasClass("glyphicon-ok")) {
             $("#namebox").removeClass("glyphicon-ok");
             $("#namediv").removeClass("has-success");
         }
         $("#namebox").addClass("glyphicon-remove");
         $("#namediv").addClass("has-error");
-        $('#namediv .alert').removeClass("disappear");
-        $('#namediv .alert').addClass("appear");
-        $('#namediv .alert').text(data.errormessages.name);
+        if($('#namealert').hasClass("hidden"))
+            $('#namealert').removeClass("hidden");
+        $('#namealert').removeClass("disappear");
+        $('#namealert').addClass("appear");
+        $('#namealert').text(data.errormessages.name);
     } else {
-        if($("#namebox").hasClass("glyphicon-remove")){
+        if ($("#namebox").hasClass("glyphicon-remove")) {
             $("#namebox").removeClass("glyphicon-remove");
             $("#namediv").removeClass("has-error");
         }
         $("#namebox").addClass("glyphicon-ok");
         $("#namediv").addClass("has-success");
-        $('#namediv .alert').removeClass("appear");
-        $('#namediv .alert').addClass("disappear");
+        $('#namealert').removeClass("appear");
+        $('#namealert').addClass("disappear");
+
     }
     if (data.errors.email) {
-        if($("#emailbox").hasClass("glyphicon-ok")){
+        if ($("#emailbox").hasClass("glyphicon-ok")) {
             $("#emailbox").removeClass("glyphicon-ok");
             $("#emaildiv").removeClass("has-success");
         }
         $("#emailbox").addClass("glyphicon-remove");
         $("#emaildiv").addClass("has-error");
-        $('#emaildiv .alert').removeClass("disappear");
-        $('#emaildiv .alert').addClass("appear");
-        $('#emaildiv .alert').text(data.errormessages.email);
+        if($('#emailalert').hasClass("hidden"))
+            $('#emailalert').removeClass("hidden");
+        $('#emailalert').removeClass("disappear");
+        $('#emailalert').addClass("appear");
+        $('#emailalert').text(data.errormessages.email);
     } else {
-        if($("#emailbox").hasClass("glyphicon-remove")){
+        if ($("#emailbox").hasClass("glyphicon-remove")) {
             $("#emailbox").removeClass("glyphicon-remove");
             $("#emaildiv").removeClass("has-error");
         }
         $("#emailbox").addClass("glyphicon-ok");
         $("#emaildiv").addClass("has-success");
-        $('#emaildiv .alert').removeClass("appear");
-        $('#emaildiv .alert').addClass("disappear");
+        $('#emailalert').removeClass("appear");
+        $('#emailalert').addClass("disappear");
     }
     if (data.errors.message) {
-        if($("#textbox").hasClass("glyphicon-ok")){
+        if ($("#textbox").hasClass("glyphicon-ok")) {
             $("#textbox").removeClass("glyphicon-ok");
             $("#textdiv").removeClass("has-success");
         }
         $("#textbox").addClass("glyphicon-remove");
         $("#textdiv").addClass("has-error");
-        $('#textdiv .alert').removeClass("disappear");
-        $('#textdiv .alert').addClass("appear");
-        $('#textdiv .alert').text(data.errormessages.message);
+        if($('#textalert').hasClass("hidden"))
+            $('#textalert').removeClass("hidden");
+        $('#textalert').removeClass("disappear");
+        $('#textalert').addClass("appear");
+        $('#textalert').text(data.errormessages.message);
     } else {
-        if($("#textbox").hasClass("glyphicon-remove")){
+        if ($("#textbox").hasClass("glyphicon-remove")) {
             $("#textbox").removeClass("glyphicon-remove");
             $("#textdiv").removeClass("has-error");
         }
         $("#textbox").addClass("glyphicon-ok");
         $("#textdiv").addClass("has-success");
-        $('#textdiv .alert').removeClass("appear");
-        $('#textdiv .alert').addClass("disappear");
+        $('#textalert').removeClass("appear");
+        $('#textalert').addClass("disappear");
     }
-    if(!$("#mobilbox").hasClass("glyphicon-ok")){
+    if (!$("#mobilbox").hasClass("glyphicon-ok")) {
         $("#mobilbox").addClass("glyphicon-ok");
         $("#mobildiv").addClass("has-success");
     }
 
 }
 
-function contact(event){
+function contact(event) {
     event.preventDefault();
     var formData = {
         "name": $('input[name=name]').val(),
@@ -229,10 +167,10 @@ function contact(event){
         dataType: 'json',
         data: formData,
         //contentType: 'application/json; charset=utf-8',
-        success: function (data, textStatus, jqXHR ) {
+        success: function (data/*, textStatus, jqXHR */) {
             contactajaxsuccess(data);
         },
-        error: function(jqXHR, textStatus, errorThrown ) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.log(jqXHR);
             console.log("Details: " + textStatus + "\nError:" + errorThrown);
         }
